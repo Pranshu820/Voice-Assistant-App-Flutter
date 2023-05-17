d9c1e29873c3efc15ec933b385529f20e8a843f9
Pranshu <75555961+Pranshu820@users.noreply.github.com>
2023-05-17 22:08:51 +0530

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,2 +1,1 @@
-// const openAIAPIKey = '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';
-const openAIAPIKey = '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';
+const openAIAPIKey = 'Your API Key';
